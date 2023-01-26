a707d77f582c96b82e904502a8fb4d09f1159ea3
Anna Sisenina <yuta4ka71@gmail.com>
2023-01-26 19:51:43 +0300

    HomeWork TwoFriend added

---
+++ HomeWork_TwoFriends/Program.cs
@@ -0,0 +1,23 @@
+﻿double distance=1000, 
+       firstFriendSpeed=1, 
+       secondFriendSpeed=2, 
+       dogSpeed=5;
+double time=0; 
+int count = 0; 
+int friend=2;
+while (distance>=1)
+{
+    if (friend==2)
+    {
+        time=distance/(secondFriendSpeed+dogSpeed);
+        friend=1;
+    }
+    else 
+    {
+        time=distance/(firstFriendSpeed+dogSpeed);
+        friend=2;
+    }
+    distance=distance-(firstFriendSpeed+secondFriendSpeed)*time;
+    count++;
+}
+Console.WriteLine(count);
